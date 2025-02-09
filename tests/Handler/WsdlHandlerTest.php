453876a2daab8546f4352abc0eb6f5cd8c02e931
Frank Rakow <frank.rakow@gmail.com>
2025-02-09 17:39:56 +0100

    Add SSL verification support to WsdlHandler tests
    Introduce an `Environment` mock in `WsdlHandlerTest` to configure SSL verification settings. Added a new test case to validate `WsdlHandler` behavior with SSL peer and host verification enabled. This ensures better coverage for scenarios involving HTTPS WSDL fetching.

--- tests/Handler/WsdlHandlerTest.php
+++ tests/Handler/WsdlHandlerTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace MonoSize\SoapProxy\Tests\Handler;
 
 use MonoSize\SoapProxy\Cache\WsdlCache;
+use MonoSize\SoapProxy\Config\Environment;
 use MonoSize\SoapProxy\Handler\WsdlHandler;
 use MonoSize\SoapProxy\Request\SoapProxyRequest;
 use PHPUnit\Framework\TestCase;
@@ -13,11 +14,9 @@ use Psr\Log\LoggerInterface;
 class WsdlHandlerTest extends TestCase
 {
     private $request;
-
     private $logger;
-
     private $cache;
-
+    private $environment;
     private $handler;
 
     protected function setUp(): void
@@ -25,11 +24,21 @@ class WsdlHandlerTest extends TestCase
         $this->request = $this->createMock(SoapProxyRequest::class);
         $this->logger = $this->createMock(LoggerInterface::class);
         $this->cache = $this->createMock(WsdlCache::class);
+        $this->environment = $this->createMock(Environment::class);
+
+        // Configure default environment behavior
+        $this->environment
+            ->method('get')
+            ->willReturnMap([
+                ['SSL_VERIFY_PEER', 'false', 'false'],
+                ['SSL_VERIFY_HOST', 'false', 'false']
+            ]);
 
         $this->handler = new WsdlHandler(
             $this->request,
             $this->logger,
-            $this->cache
+            $this->cache,
+            $this->environment
         );
     }
 
@@ -74,5 +83,44 @@ class WsdlHandlerTest extends TestCase
         $this->expectException(\RuntimeException::class);
 
         $this->handler->handle();
+    }
+
+    public function testHandleWithSSLVerification(): void
+    {
+        // Configure environment to enable SSL verification
+        $environment = $this->createMock(Environment::class);
+        $environment
+            ->method('get')
+            ->willReturnMap([
+                ['SSL_VERIFY_PEER', 'false', 'true'],
+                ['SSL_VERIFY_HOST', 'false', 'true']
+            ]);
+
+        $handler = new WsdlHandler(
+            $this->request,
+            $this->logger,
+            $this->cache,
+            $environment
+        );
+
+        $this->request
+            ->expects($this->once())
+            ->method('getTargetUrl')
+            ->willReturn('https://example.com/service?wsdl');
+
+        $this->cache
+            ->expects($this->once())
+            ->method('getCached')
+            ->willReturn(null);
+
+        $this->request
+            ->expects($this->once())
+            ->method('getCredentials')
+            ->willReturn(['user' => 'test', 'pass' => 'test']);
+
+        // The test will fail with RuntimeException since we can't make actual HTTPS calls in tests
+        $this->expectException(\RuntimeException::class);
+
+        $handler->handle();
     }
 }
